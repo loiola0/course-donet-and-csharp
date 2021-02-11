55ebe51b23989b807faef18b12ba431a6caeb087
loiola0 <victorloiola@alu.ufc.br>
2021-02-11 13:12:35 -0300

    primeira parte do terceiro modulo

---
+++ módulo-02/DigitalInnovationOne/Pilha.cs
@@ -0,0 +1,42 @@
+using System;
+
+
+namespace DigitalInnovationOne{
+
+    public class Pilha {
+
+        Posicao primeiro;
+
+            
+            public void Empilhar(object item){
+                primeiro = new Posicao(primeiro,item);
+            }
+
+            public object Desempilhar(){
+                
+                if(primeiro == null){
+                    throw new InvalidOperationException();
+                }
+
+                object resultado = primeiro.item;
+                primeiro = primeiro.proximo;
+                return resultado;
+
+            }
+
+            class Posicao{
+                public Posicao proximo;
+                
+                public object item;
+
+                public Posicao(Posicao proximo, object item){
+                    this.proximo = proximo;
+                    this.item = item;
+                    
+                }
+            }
+
+
+    }
+    
+}
---
+++ módulo-02/DigitalInnovationOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroVezes = 10;
+
+                for (int i = 0; i < numeroVezes; i++){
+                    Console.WriteLine($"Olá pai {i}");    
+                }
+        }
+    }
+}
