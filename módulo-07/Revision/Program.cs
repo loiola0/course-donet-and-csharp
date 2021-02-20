a75437493ef7965059b480278f39e3df9468b0c9
loiola0 <victorloiola@alu.ufc.br>
2021-02-19 23:01:07 -0300

    primeira aula do modulo 07 concluida

--- módulo-07/Revision/Program.cs
+++ módulo-07/Revision/Program.cs
@@ -4,9 +4,45 @@ namespace Revision
 {
     class Program
     {
+
+        static string Menu(){
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular médial geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine("");
+
+            string opcaoUser = Console.ReadLine();
+            
+            return opcaoUser;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUser = Menu();
+
+            while(opcaoUser.ToUpper() != "X"){
+                switch(opcaoUser){
+                    case "1":
+                        //TODO: adicionar aluno.
+                        break;
+                    case "2":
+                        //TODO: listar alunos.
+                        break;
+                    case "3":
+                        //TODO: calcular média geral.
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                //chamada do menu
+                opcaoUser = Menu();
+
+                
+            }
+
+
         }
     }
 }
